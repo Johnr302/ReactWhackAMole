e773b742556aa372bfece3a197462d0b8df3533f
Johnr302 <johnathon.regalado@gmail.com>
2020-08-24 13:27:25 -0700

    yarn build

---
+++ build/precache-manifest.452e55e0a33c030bd301e4e8b02f658c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86083a1cabf1dda066b2c157448bb5e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cfa764d1de7476eb367",
+    "url": "/static/css/main.e54a6fcd.chunk.css"
+  },
+  {
+    "revision": "3c2968ef8b2eb466d66b",
+    "url": "/static/js/2.32bdae52.chunk.js"
+  },
+  {
+    "revision": "1cfa764d1de7476eb367",
+    "url": "/static/js/main.b33d546d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d1f1421962be639557b87a3e7de1954",
+    "url": "/static/media/mole.8d1f1421.png"
+  }
+]);
