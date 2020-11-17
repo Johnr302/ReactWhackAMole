f515fb14dc3645748c453172bdf37b1301bd33d9
Pjohn <50125720+Johnr302@users.noreply.github.com>
2020-11-16 17:55:44 -0800

    Update Timer.test.js

--- test/Timer.test.js
+++ test/Timer.test.js
@@ -7,12 +7,12 @@ import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Timer />", () => {
-  it("renders <Timer /> components", () => {
+  it("renders <Timer /> component", () => {
     const wrapper = shallow(<Timer />);
     expect(wrapper.find("h3")).toHaveLength(1);
   });
 
-  it("renders a <Timer /> components with a number", () => {
+  it("renders a <Timer /> component with a number", () => {
     const wrapper = shallow(<Timer />);
     console.log(wrapper.text());
     expect(wrapper.text()).toEqual("Timer: 0 Start Game");
