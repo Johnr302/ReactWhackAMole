6ff3a3826c48bd03af79e66e06ee79bbf62b58f3
Johnr302 <johnathon.regalado@gmail.com>
2020-11-16 19:22:06 -0800

    Added test for GameOver compenent to verify the correct message comes up with a given score

--- test/GameOver.test.js
+++ test/GameOver.test.js
@@ -12,8 +12,19 @@ describe("<GameOver />", () => {
     expect(wrapper.find("#gameOver")).toHaveLength(1);
   });
 
-  it("renders a <GameOver /> component", () => {
+  it(`renders a <GameOver /> component with "Participation Trophy" message if
+    score is between 0 and 50`, () => {
     const wrapper = shallow(<GameOver score={3} />);
     expect(wrapper.text()).toEqual("Participation trophy");
+  });
+  it(`renders a <GameOver /> component with "Pretty Good" message if
+    score is between 49 and 100`, () => {
+    const wrapper = shallow(<GameOver score={50} />);
+    expect(wrapper.text()).toEqual("Pretty Good");
+  });
+  it(`renders a <GameOver /> component with "Good Job" message if
+    score is >= 100`, () => {
+    const wrapper = shallow(<GameOver score={101} />);
+    expect(wrapper.text()).toEqual("Good Job!");
   });
 });
